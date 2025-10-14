f9f06d89974fc9b551f15970365246853432fc5b
jagnyadattadcs <raj90.dcs@gmail.com>
2025-10-14 18:49:13 +0530

    Added api to the backend route

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
-export const BackenUrl='https://chandramarealconbackend-wsct.onrender.com';
+export const BackenUrl='https://chandramarealconbackend-wsct.onrender.com/api';
 // export const BackenUrl = "http://localhost:5000/api";
